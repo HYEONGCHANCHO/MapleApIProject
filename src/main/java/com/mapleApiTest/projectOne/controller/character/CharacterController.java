a484d8891620171e7009906cdfe0e273036d50b9
HYEONGCHANCHO <wjstktpghd2@naver.com>
2024-02-01 17:02:29 +0900

    maplestory OpenApi 적용 확인

--- src/main/java/com/mapleApiTest/projectOne/controller/character/CharacterController.java
+++ src/main/java/com/mapleApiTest/projectOne/controller/character/CharacterController.java
@@ -2,28 +2,59 @@ package com.mapleApiTest.projectOne.controller.character;
 
 import com.mapleApiTest.projectOne.dto.character.request.CharacterCreateRequest;
 import com.mapleApiTest.projectOne.service.character.CharacterService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class CharacterController {
-    private final CharacterService characterService;
 
-    public CharacterController(CharacterService characterService) {
-        this.characterService = characterService;
+    @Value("${external.api.key}")
+    private String apiKey;
+
+
+//    @GetMapping("/maplestory/v1/id")
+//    public String getOcid(@RequestParam String characterName) {
+//    return characterName;
+//    }
+    @GetMapping("/maplestory/v1/id")
+    public ResponseEntity<String> getOcid(@RequestParam String characterName) {
+
+        try {
+            String apiUrl = "https://open.api.nexon.com/maplestory/v1/id";
+
+            String fullUrl = UriComponentsBuilder.fromUriString(apiUrl).queryParam("character_name", characterName).build().toUriString();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("x-nxopen-api-key", apiKey);
+
+            ResponseEntity<String> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+            return responseEntity;
+        } catch (Exception exception) {
+            return ResponseEntity.status(500).body("에러 발생: " + exception.getMessage());
+        }
     }
+
+
+//    private final CharacterService characterService;
+//
+//    public CharacterController(CharacterService characterService) {
+//        this.characterService = characterService;
+//    }
 
 //    @PostMapping("/characters")
 //    public void saveCharacter(@RequestBody CharacterCreateRequest request) {
 //        characterService.saveCharacter(request);
 //    }
 //
-    @GetMapping("https://open.api.nexon.com/maplestory/v1/id")
-    public String getCharacterOcid(@RequestBody CharacterCreateRequest request){
-        return;
-    }
+//    @GetMapping("https://open.api.nexon.com/maplestory/v1/id")
+//    public String getCharacterOcid(@RequestBody CharacterCreateRequest request){
+//        return;
+//    }
 
 }
--- src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
+++ src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
@@ -1,8 +1,8 @@
 package com.mapleApiTest.projectOne.service.character;
 
-import com.mapleApiTest.projectOne.domain.character.Characters;
+//import com.mapleApiTest.projectOne.domain.character.Characters;
 import com.mapleApiTest.projectOne.dto.character.request.CharacterCreateRequest;
-import com.mapleApiTest.projectOne.repository.character.CharacterRepository;
+//import com.mapleApiTest.projectOne.repository.character.CharacterRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,10 +20,10 @@ public class CharacterService {
 //        characterRepository.save(new Characters(request.getName()));
 //    }
 
-    @Transactional
-    public String CharactersResponse getCharacters(){
-        return ;
-    }
+//    @Transactional
+//    public String CharactersResponse getCharacters(){
+//        return ;
+//    }
 
 
 }
