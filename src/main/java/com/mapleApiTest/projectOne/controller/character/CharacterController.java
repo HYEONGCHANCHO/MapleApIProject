43278ae2c9285b133354c0d325737f908a4f5304
HYEONGCHANCHO <wjstktpghd2@naver.com>
2024-02-15 17:50:10 +0900

    webclient로 수정 작업

--- src/main/java/com/mapleApiTest/projectOne/controller/character/CharacterController.java
+++ src/main/java/com/mapleApiTest/projectOne/controller/character/CharacterController.java
@@ -2,7 +2,7 @@ package com.mapleApiTest.projectOne.controller.character;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.util.concurrent.RateLimiter;
-import com.mapleApiTest.projectOne.dto.character.request.GetChracterInfo;
+
 import com.mapleApiTest.projectOne.dto.character.request.GetCharactersOcid;
 import com.mapleApiTest.projectOne.dto.character.response.CharacterInfo;
 import com.mapleApiTest.projectOne.service.character.CharacterService;
@@ -13,7 +13,9 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -33,57 +35,104 @@ public class CharacterController {
         this.characterService = characterService;
     }
 
+
+//
+//    @GetMapping("/maplestory/v1/id")
+//    public Mono<String> yourApiEndpoint(@RequestParam String charactersName) {
+//        return characterService.getApiResponse(charactersName);
+//    }
+
+//
+//    @GetMapping("/maplestory/v1/id")
+//    public Mono<ResponseEntity<String>> getCharacterOcid(HttpServletRequest request, @RequestParam String charactersName) {
+//
+//        if (rateLimiter.tryAcquire()) {
+//            GetCharactersOcid getCharactersOcid = new GetCharactersOcid(charactersName);
+//            String url = request.getRequestURI();
+//            System.out.println("여기여기");
+//            System.out.println(url);
+//
+//            return characterService.getApiResponse(getCharactersOcid.getName())
+//                    .map(ResponseEntity::ok);
+//
+////
+////                    return characterService.getCharacterOcid(getCharactersOcid,url)
+////                    .map(ResponseEntity::ok);
+//            // 비동기적으로 getCharacterOcidAsync 메소드 호출
+////           String resultMono = characterService.getCharacterOcid(getCharactersOcid, url);
+////
+////            System.out.println(url+"ㅇㄴㅇ"+resultMono);
+////
+////            // 작업이 완료될 때까지 대기하고 결과를 ResponseEntity로 감싸서 반환
+////            return resultMono;
+//        } else {
+//            // 초당 호출 제한 초과 시 429 Too Many Requests 반환
+//            return null;
+//        }
+//    }
+//
+//
+//
+
+
+//        @GetMapping("/maplestory/v1/id")
+//    public Mono<String> yourApiEndpoint(@RequestParam String charactersName) {
+////        return characterService.getApiResponse(charactersName);
+//        return characterService.getCharacterOcid(charactersName);
+//    }
     @GetMapping("/maplestory/v1/id")
-    public CompletableFuture<ResponseEntity<String>> getCharacterOcid(@RequestParam String charactersName) {
+    public Mono<String> getCharacterOcid(HttpServletRequest request, @RequestParam String charactersName) {
 
         if (rateLimiter.tryAcquire()) {
         GetCharactersOcid getCharactersOcid = new GetCharactersOcid(charactersName);
-        String url = apiUrl + "/maplestory/v1/id";
+        String Url = request.getRequestURI();
         System.out.println("여기여기");
+        System.out.println(Url);
+            return characterService.getCharacterOcid(getCharactersOcid,Url);
+
+//                    Mono<String> resultMono = characterService.getCharacterOcid(charactersName);
+
 
         // 비동기적으로 getCharacterOcidAsync 메소드 호출
-        CompletableFuture<String> resultFuture = characterService.getCharacterOcid(getCharactersOcid, url, apiKey);
-
+//        Mono<String> resultMono = characterService.getCharacterOcid(getCharactersOcid, url);
         // 작업이 완료될 때까지 대기하고 결과를 ResponseEntity로 감싸서 반환
-        return resultFuture.thenApply(ResponseEntity::ok);
+//        return resultMono;
         } else {
             // 초당 호출 제한 초과 시 429 Too Many Requests 반환
-            return CompletableFuture.completedFuture(ResponseEntity.status(429).build());
+            return null;
         }
-
-
     }
 
-    @GetMapping("/maplestory/v1/character/basic")
-    @Async("characterThreadPool")
-
-    public CompletableFuture<ResponseEntity<CharacterInfo>> getCharacterInfo(@RequestParam String ocid, String date) {
-        CompletableFuture<ResponseEntity<CharacterInfo>> resultFuture = new CompletableFuture<>();
-
-        if (rateLimiter.tryAcquire()) {
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                GetChracterInfo getChracterInfo = new GetChracterInfo(date, ocid);
-                String Url = apiUrl + "/maplestory/v1/character/basic";
-
-                System.out.println("여기여기222");
-
-
-                resultFuture.complete(ResponseEntity.ok(characterService.getCharacterInfo(getChracterInfo, Url, apiKey, ocid)));
-            } catch (Exception exception) {
-                System.err.println("에러: " + exception.getMessage());
-                return null;
-            }
-        }else {
-                System.err.println("호출 제한 초과");
-            resultFuture.complete(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build());
-
-        }
-        System.out.println("resultFuture ::"+ resultFuture);
-
-        return resultFuture;
-
-
-}
+//    @GetMapping("/maplestory/v1/character/basic")
+//    @Async("characterThreadPool")
+//
+//    public CompletableFuture<ResponseEntity<CharacterInfo>> getCharacterInfo(@RequestParam String ocid, String date) {
+//        CompletableFuture<ResponseEntity<CharacterInfo>> resultFuture = new CompletableFuture<>();
+//
+//        if (rateLimiter.tryAcquire()) {
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            try {
+//                GetChracterInfo getChracterInfo = new GetChracterInfo(date, ocid);
+//                String Url = apiUrl + "/maplestory/v1/character/basic";
+//
+//                System.out.println("여기여기222");
+//
+//
+//                resultFuture.complete(ResponseEntity.ok(characterService.getCharacterInfo(getChracterInfo, Url, apiKey, ocid)));
+//            } catch (Exception exception) {
+//                System.err.println("에러: " + exception.getMessage());
+//                return null;
+//            }
+//        }else {
+//                System.err.println("호출 제한 초과");
+//            resultFuture.complete(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build());
+//
+//        }
+//        System.out.println("resultFuture ::"+ resultFuture);
+//
+//        return resultFuture;
+//
+//
+//}
 }
--- src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
+++ src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
@@ -13,12 +13,16 @@ import com.mapleApiTest.projectOne.dto.character.response.CharacterInfo;
 import com.mapleApiTest.projectOne.repository.character.CharactersInfoRepository;
 import com.mapleApiTest.projectOne.repository.character.CharactersKeyRepository;
 import com.mapleApiTest.projectOne.repository.character.CharactersStatInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -31,101 +35,154 @@ public class CharacterService {
     private final CharactersStatInfoRepository charactersStatInfoRepository;
 
 
-    public CharacterService(CharactersKeyRepository charactersKeyRepository, CharactersInfoRepository charactersInfoRepository, CharactersStatInfoRepository charactersStatInfoRepository) {
+    private final WebClient webClient;
+
+    @Value("${external.api.key}")
+    private String apiKey;
+    @Value("${external.api.url}")
+    private String apiUrl;
+
+
+
+
+//    public CharacterService(CharactersKeyRepository charactersKeyRepository, CharactersInfoRepository charactersInfoRepository, CharactersStatInfoRepository charactersStatInfoRepository) {
+//        this.charactersKeyRepository = charactersKeyRepository;
+//        this.charactersInfoRepository = charactersInfoRepository;
+//        this.charactersStatInfoRepository = charactersStatInfoRepository;
+//    }
+
+//    public CharacterService(WebClient.Builder builder,CharactersKeyRepository charactersKeyRepository, CharactersInfoRepository charactersInfoRepository, CharactersStatInfoRepository charactersStatInfoRepository) {
+//        this.webClient = builder
+//                .filter((request, next) -> next.exchange(request))
+//                .defaultHeader("x-nxopen-api-key", apiKey)
+//                .baseUrl(apiUrl)
+//                .build();
+//        this.charactersKeyRepository = charactersKeyRepository;
+//        this.charactersInfoRepository = charactersInfoRepository;
+//        this.charactersStatInfoRepository = charactersStatInfoRepository;
+//    }
+    public CharacterService(WebClient.Builder builder,CharactersKeyRepository charactersKeyRepository, CharactersInfoRepository charactersInfoRepository, CharactersStatInfoRepository charactersStatInfoRepository,   @Value("${external.api.key}") String apiKey,
+                            @Value("${external.api.url}") String apiUrl) {
+        this.webClient = builder
+                .defaultHeader("x-nxopen-api-key", apiKey)
+                .baseUrl(apiUrl)
+                .build();
         this.charactersKeyRepository = charactersKeyRepository;
         this.charactersInfoRepository = charactersInfoRepository;
         this.charactersStatInfoRepository = charactersStatInfoRepository;
     }
 
     @Async("characterThreadPool")
-    public CompletableFuture<String> getCharacterOcid(GetCharactersOcid request, String Url, String apiKey) {
+    public Mono<String> getCharacterOcid(GetCharactersOcid request, String Url) {
         Optional<CharactersKey> charactersKeyOptional = charactersKeyRepository.findByCharactersName(request.getName());
-
+        System.out.println("여기여기2");
         if (charactersKeyOptional.isPresent()) {
             CharactersKey charactersKey = charactersKeyOptional.get();
             String ocidValue = charactersKey.getOcid();
-            // ocidValue를 사용하거나 처리
             System.out.println("Found ocid: " + ocidValue);
-            return CompletableFuture.completedFuture(ocidValue);
+            return Mono.just(ocidValue);
         } else {
-            try {
-                String fullUrl = UriComponentsBuilder.fromUriString(Url).queryParam("character_name", request.getName()).build().toUriString();
-                HttpHeaders headers = new HttpHeaders();
-                headers.set("x-nxopen-api-key", apiKey);
-                ResponseEntity<String> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+            System.out.println("여기여기3");
+            return webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(Url)
+                            .queryParam("character_name",request.getName())
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .flatMap(responseBody -> {
+                        System.out.println("여기여기7");
+                        try {
+                            System.out.println("ㅇㅇㅇㅇ3");
 
-                String responseBody = responseEntity.getBody();
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode jsonNode = objectMapper.readTree(responseBody);
-                String ocidValue = jsonNode.get("ocid").asText();
-                charactersKeyRepository.save(new CharactersKey(request.getName(), ocidValue));
+                            ObjectMapper objectMapper = new ObjectMapper();
+                            JsonNode jsonNode = objectMapper.readTree(responseBody);
+                            String ocidValue = jsonNode.get("ocid").asText();
+                            charactersKeyRepository.save(new CharactersKey(request.getName(), ocidValue));
+                            System.out.println("여기여기4");
 
-                return CompletableFuture.completedFuture(ocidValue);
-            } catch (Exception exception) {
-                System.err.println("에러: " + exception.getMessage());
-                return CompletableFuture.completedFuture(null);
-            }
+                            return Mono.just(ocidValue);
+                        } catch (Exception exception) {
+                            System.out.println("ㅇㅇㅇㅇ2");
+
+                            System.err.println("에러: " + exception.getMessage());
+                            return Mono.error(exception);
+                        }
+                    })
+                    .onErrorResume(exception -> {
+                        System.out.println("ㅇㅇㅇㅇ");
+
+                        System.err.println("에러: " + exception.getMessage());
+                        exception.printStackTrace(); // 추가된 부분
+                        return Mono.error(exception);
+
+                    });
+
         }
-    }
-    public CompletableFuture<String> getCharactersInfo(GetCharactersInfo request, String Url, String apiKey, String ocid) {
-
-        Optional<CharactersInfo> charactersInfoOptional = charactersInfoRepository.findByCharactersNameAndDate(request.getCharactersName(),request.getDate());
-
-        if (charactersInfoOptional.isPresent()) {
-            CharactersInfo charactersInfo = charactersInfoOptional.get();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonResult;
-            try {
-                jsonResult = objectMapper.writeValueAsString(charactersInfo);
-            } catch (Exception e) {
-                // JSON 변환 중 오류 처리
-                e.printStackTrace();
-                return CompletableFuture.completedFuture("Error occurred during JSON conversion");
-            }
-
-            // JSON 문자열을 CompletableFuture로 감싸서 반환
-            return CompletableFuture.completedFuture(jsonResult);
-
-        } else {
 
 
-        try {
-
-            String fullUrl = UriComponentsBuilder.fromUriString(Url).queryParam("ocid", ocid).queryParam("date", request.getDate()).build().toUriString();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("x-nxopen-api-key", apiKey);
-
-            ResponseEntity<CharactersInfo> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharactersInfo.class);
-            // 서버 응답에서 받은 CharacterInfo
-            CharactersInfo jsonResult = responseEntity.getBody();
-
-            // "ocid" 값을 클라이언트에서 원하는 값으로 설정
-
-            return CompletableFuture.completedFuture(jsonResult);
+    }}
 
 
-//            return responseEntity.getBody();
-        } catch (HttpClientErrorException e) {
-            HttpStatus statusCode = e.getStatusCode();
-            String responseBody = e.getResponseBodyAsString();
+//    public CompletableFuture<String> getCharactersInfo(GetCharactersInfo request, String Url, String apiKey, String ocid) {
+//
+//        Optional<CharactersInfo> charactersInfoOptional = charactersInfoRepository.findByCharactersNameAndDate(request.getCharactersName(),request.getDate());
+//
+//        if (charactersInfoOptional.isPresent()) {
+//            CharactersInfo charactersInfo = charactersInfoOptional.get();
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String jsonResult;
+//            try {
+//                jsonResult = objectMapper.writeValueAsString(charactersInfo);
+//            } catch (Exception e) {
+//                // JSON 변환 중 오류 처리
+//                e.printStackTrace();
+//                return CompletableFuture.completedFuture("Error occurred during JSON conversion");
+//            }
+//
+//            // JSON 문자열을 CompletableFuture로 감싸서 반환
+//            return CompletableFuture.completedFuture(jsonResult);
+//
+//        } else {
+//
+//
+//        try {
+//
+//            String fullUrl = UriComponentsBuilder.fromUriString(Url).queryParam("ocid", ocid).queryParam("date", request.getDate()).build().toUriString();
+//
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.set("x-nxopen-api-key", apiKey);
+//
+//            ResponseEntity<CharactersInfo> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharactersInfo.class);
+//            // 서버 응답에서 받은 CharacterInfo
+//            CharactersInfo jsonResult = responseEntity.getBody();
+//
+//            // "ocid" 값을 클라이언트에서 원하는 값으로 설정
+//
+//            return CompletableFuture.completedFuture(jsonResult);
+//
+//
+////            return responseEntity.getBody();
+//        } catch (HttpClientErrorException e) {
+//            HttpStatus statusCode = e.getStatusCode();
+//            String responseBody = e.getResponseBodyAsString();
+//
+//            System.err.println("HTTP 상태 코드: " + statusCode);
+//            System.err.println("응답 본문: " + responseBody);
+//            return null;
+//
+//        } catch (Exception exception) {
+//            System.err.println("에러: " + exception.getMessage());
+//
+//            return null;
+//        }
+//    }
+//
 
-            System.err.println("HTTP 상태 코드: " + statusCode);
-            System.err.println("응답 본문: " + responseBody);
-            return null;
-
-        } catch (Exception exception) {
-            System.err.println("에러: " + exception.getMessage());
-
-            return null;
-        }
-    }
 
 
-
-
-}
+//}
 
 
 
