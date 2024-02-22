d867a67516d877f42b2e37d5c92e75bed22f8dc6
HYEONGCHANCHO <wjstktpghd2@naver.com>
2024-02-22 17:29:28 +0900

    service 코드 오류 수정

--- src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
+++ src/main/java/com/mapleApiTest/projectOne/service/character/CharacterService.java
@@ -89,56 +89,56 @@ public class CharacterService {
 
 /////////////////////////////////////////////////
 
-    public CompletableFuture<String> getCharactersInfo(GetCharactersInfo request, String Url, String apiKey, String ocid) {
-
-        Optional<CharactersInfo> charactersInfoOptional = charactersInfoRepository.findByCharactersNameAndDate(request.getCharactersName(), request.getDate());
-
-        if (charactersInfoOptional.isPresent()) {
-            CharactersInfo charactersInfo = charactersInfoOptional.get();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonResult;
-            try {
-                jsonResult = objectMapper.writeValueAsString(charactersInfo);
-            } catch (Exception e) {
-                // JSON 변환 중 오류 처리
-                e.printStackTrace();
-                return CompletableFuture.completedFuture("Error occurred during JSON conversion");
-            }
-            // JSON 문자열을 CompletableFuture로 감싸서 반환
-            return CompletableFuture.completedFuture(jsonResult);
-
-        } else {
-            try {
-                String fullUrl = UriComponentsBuilder.fromUriString(Url).queryParam("ocid", ocid).queryParam("date", request.getDate()).build().toUriString();
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.set("x-nxopen-api-key", apiKey);
-
-                ResponseEntity<CharactersInfo> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharactersInfo.class);
-                // 서버 응답에서 받은 CharacterInfo
-                CharactersInfo jsonResult = responseEntity.getBody();
-
-                // "ocid" 값을 클라이언트에서 원하는 값으로 설정
-
-                return null;
-//                return CompletableFuture.completedFuture(jsonResult);
-
-//            return responseEntity.getBody();
-            } catch (HttpClientErrorException e) {
-                HttpStatus statusCode = e.getStatusCode();
-                String responseBody = e.getResponseBodyAsString();
-
-                System.err.println("HTTP 상태 코드: " + statusCode);
-                System.err.println("응답 본문: " + responseBody);
-                return null;
-
-            } catch (Exception exception) {
-                System.err.println("에러: " + exception.getMessage());
-                return null;
-            }
-        }
-    }
+//    public CompletableFuture<String> getCharactersInfo(GetCharactersInfo request, String Url, String apiKey, String ocid) {
+//
+//        Optional<CharactersInfo> charactersInfoOptional = charactersInfoRepository.findByCharactersNameAndDate(request.getCharactersName(), request.getDate());
+//
+//        if (charactersInfoOptional.isPresent()) {
+//            CharactersInfo charactersInfo = charactersInfoOptional.get();
+//
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String jsonResult;
+//            try {
+//                jsonResult = objectMapper.writeValueAsString(charactersInfo);
+//            } catch (Exception e) {
+//                // JSON 변환 중 오류 처리
+//                e.printStackTrace();
+//                return CompletableFuture.completedFuture("Error occurred during JSON conversion");
+//            }
+//            // JSON 문자열을 CompletableFuture로 감싸서 반환
+//            return CompletableFuture.completedFuture(jsonResult);
+//
+//        } else {
+//            try {
+//                String fullUrl = UriComponentsBuilder.fromUriString(Url).queryParam("ocid", ocid).queryParam("date", request.getDate()).build().toUriString();
+//
+//                HttpHeaders headers = new HttpHeaders();
+//                headers.set("x-nxopen-api-key", apiKey);
+//
+//                ResponseEntity<CharactersInfo> responseEntity = new RestTemplate().exchange(fullUrl, HttpMethod.GET, new HttpEntity<>(headers), CharactersInfo.class);
+//                // 서버 응답에서 받은 CharacterInfo
+//                CharactersInfo jsonResult = responseEntity.getBody();
+//
+//                // "ocid" 값을 클라이언트에서 원하는 값으로 설정
+//
+//                return null;
+////                return CompletableFuture.completedFuture(jsonResult);
+//
+////            return responseEntity.getBody();
+//            } catch (HttpClientErrorException e) {
+//                HttpStatus statusCode = e.getStatusCode();
+//                String responseBody = e.getResponseBodyAsString();
+//
+//                System.err.println("HTTP 상태 코드: " + statusCode);
+//                System.err.println("응답 본문: " + responseBody);
+//                return null;
+//
+//            } catch (Exception exception) {
+//                System.err.println("에러: " + exception.getMessage());
+//                return null;
+//            }
+//        }
+//    }
 }
 
 
